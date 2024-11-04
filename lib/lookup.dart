cf38a639f9e953120517c82dcb304fb30ecfb688
gmpassos <gracilianomp@gmail.com>
2024-11-04 04:05:41 -0300

    ✨♻️ Add length property to LookupMap
    🔄 refactor(lib): add method to list all entries and delete key
    
    ✨🐛 feat: add MutableLookupMap interface with putValue method

--- lib/lookup.dart
+++ lib/lookup.dart
@@ -2,6 +2,9 @@ import 'dart:typed_data';
 
 /// A data structure that maps binary keys to binary values.
 abstract class LookupMap {
+  /// The entries length. See [listEntries].
+  int get length;
+
   /// Returns the corresponding value of [key].
   ///
   /// Returns `null` when [key] is not part of the data structure.
@@ -9,4 +12,17 @@ abstract class LookupMap {
 
   /// List keys that are part of the data structure.
   Iterable<Uint8List> listKeys();
+
+  /// List all entries. See [listKeys].
+  Iterable<MapEntry<Uint8List, Uint8List>> listEntries() =>
+      listKeys().map((key) => MapEntry(key, getValue(key)!));
+
+  /// Mark a key as deleted and return it's value.
+  Uint8List? delete(List<int> key);
+}
+
+/// A Mutable [LookupMap]
+abstract class MutableLookupMap extends LookupMap {
+  /// Puts an entry, overwriting the current key or appending it.
+  Uint8List? putValue(List<int> key, List<int> value);
 }
